b8127cf4c8b6e9a6b992510068340e968e8bc617
MasterUNG <phrombutr@gmail.com>
2017-07-25 13:51:56 +0700

    DataType

--- VariableAndConstant.playground/Contents.swift
+++ VariableAndConstant.playground/Contents.swift
@@ -13,3 +13,46 @@ strName = "Nopita"
 //Implicit Varable Type let คือการประกาศค่าคงที่ ที่ไม่สามารถแก้ไขค่าได้
 let strSurname = "ABC"
 //strSurname = "DEF"    Error
+
+//Integer
+let intNumber = 123
+//intNumber= 456
+
+//Double
+let douNumber = 3.1416
+
+//Bool
+var bolStatus = true
+bolStatus = false
+
+
+//Explicit  คือการกำหนด Data Type ==> Variable
+let strStudent: String = "Somchai"
+
+let intNumber2: Int = 1
+
+var douNumber2: Double = 123.456
+douNumber2 = 456.34
+
+let bolStatus2: Bool = false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
