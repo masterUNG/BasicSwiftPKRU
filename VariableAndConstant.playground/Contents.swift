dc0550b1761fcef8c45367cda372e6ac4f2079d2
MasterUNG <phrombutr@gmail.com>
2017-07-25 13:35:25 +0700

    Variable

---
+++ VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// This is Comment
+// นี่คือหมายเหตุ
+
+//Implicit
+//Implicit Variable Type var คือการประกาศตัวแปร ที่สามารถแก้ไขค่าได้
+var strName = "Doramon" //Implicit Value Doramon ==> strName
+strName = "Nopita"
+
+//Implicit Varable Type let คือการประกาศค่าคงที่ ที่ไม่สามารถแก้ไขค่าได้
+let strSurname = "ABC"
+//strSurname = "DEF"    Error
