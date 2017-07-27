b473594999deacb24d30580cc4b79000ad1bbc19
MasterUNG <phrombutr@gmail.com>
2017-07-27 09:55:45 +0700

    Test

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var intNumber: Int?
+
+print("intNumber ==> \(String(describing: intNumber))")
+
+//กำหนดค่าเริ่มต้นให้ strName มีค่าเท่ากับ อักษร 5 ทำงานได้ไม่มีปัญหา
+var strName: String = "5"
+
+//กำหนดค่าให้ strName ใหม่ เป็นอักษร Five จะเกิดการ Error
+//เพราะ Int(Five) จะมีค่า เป็น nil
+strName = "Five"
+
+strName = "5"
+
+//วีธีการแก้ ปัญหา จะทำอย่างไร ? ให้ Operated สามารถเอาค่า nil ไปทำงานแล้วไม่ Error
+
+//Make Operated
+let intName = Int(strName)
+
+if let intNumber = intName {
+    let intAnswer = intNumber * 5
+}
+
+
+
+
+
+
+
+
+
